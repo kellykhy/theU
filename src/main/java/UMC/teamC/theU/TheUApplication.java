7402ee8e430f6510c66edb181bde84bff8a698aa
yuntasha <jaljayo85@gmail.com>
2024-01-04 18:33:26 +0900

    init

---
+++ src/main/java/UMC/teamC/theU/TheUApplication.java
@@ -0,0 +1,13 @@
+package UMC.teamC.theU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UMC/teamC/theU/TheUApplicationTests.java
@@ -0,0 +1,13 @@
+package UMC.teamC.theU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
