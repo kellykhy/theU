9cb2c649e2ac4aff49317bad5690dc26b9eeef07
BbangHo <sonsins1@naver.com>
2024-01-04 19:05:30 +0900

    setting: 초기 설정

---
+++ src/main/java/UMC/teamC/theU/apiPayload/code/BaseCode.java
@@ -0,0 +1,8 @@
+package UMC.teamC.theU.apiPayload.code;
+
+public interface BaseCode {
+
+    public ReasonDTO getReason();
+
+    public ReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/UMC/teamC/theU/apiPayload/code/ErrorReasonDTO.java
@@ -0,0 +1,19 @@
+package UMC.teamC.theU.apiPayload.code;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+public class ErrorReasonDTO {
+
+    private final boolean isSuccess;
+    private final String code;
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    public boolean getIsSuccess(){
+        return isSuccess;
+    }
+}
